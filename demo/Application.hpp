fea942f378dd5801bee35eba29f8c0f55132748b
Elviss Strazdins <elviss@elviss.lv>
2018-05-17 02:21:46 +0300

    Delete default constructors and move operators for Application

--- demo/Application.hpp
+++ demo/Application.hpp
@@ -13,6 +13,11 @@ public:
     Application(int initArgc, const char** initArgv);
     ~Application();
 
+    Application(const Application&) = delete;
+    Application& operator=(const Application&) = delete;
+    Application(Application&&) = delete;
+    Application& operator=(Application&&) = delete;
+
     bool init();
     bool run();
 
