96ea2d134bec66a3ba0de7ad800926e5d0e927a3
Elviss Strazdins <elviss@elviss.lv>
2018-08-04 13:34:43 +0300

    Remove string from Color

--- sr/Color.hpp
+++ sr/Color.hpp
@@ -6,7 +6,6 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <string>
 #include "Vector.hpp"
 
 namespace sr
@@ -29,54 +28,6 @@ namespace sr
         float b = 0.0F;
         float a = 0.0F;
 
-        static uint32_t parseColorString(const std::string& value)
-        {
-            uint32_t result = 0;
-
-            if (!value.empty())
-            {
-                if (value.front() == '#')
-                {
-                    std::string newValue(value.begin() + 1, value.end());
-
-                    size_t componentSize = (newValue.length() + 2) / 3; // get the size of component rounded up
-                    size_t newSize = componentSize * 3;
-                    newValue.resize(newSize);
-
-                    for (size_t component = 0; component < 3; ++component)
-                    {
-                        std::string currentValue;
-
-                        for (size_t byte = 0; byte < ((componentSize < 2) ? componentSize : 2); ++byte)
-                        {
-                            char c = newValue[component * componentSize + byte];
-
-                            if ((c >= '0' && c <= '9') ||
-                                (c >= 'a' && c <= 'f') ||
-                                (c >= 'A' && c <= 'F'))
-                            {
-                                currentValue += c;
-                            }
-                            else
-                            {
-                                currentValue += "0";
-                            }
-                        }
-
-                        result |= (static_cast<uint32_t>(std::stoul(currentValue, 0, 16))) << ((3 - component) * 8);
-                    }
-
-                    result |= 0xff; // alpha
-                }
-                else
-                {
-                    result = static_cast<uint32_t>(std::stoul(value));
-                }
-            }
-
-            return result;
-        }
-
         Color()
         {
         }
@@ -95,28 +46,6 @@ namespace sr
             g = static_cast<uint8_t>((color & 0x00FF0000) >> 16) / 255.0F;
             b = static_cast<uint8_t>((color & 0x0000FF00) >> 8) / 255.0F;
             a = static_cast<uint8_t>(color & 0x000000FF) / 255.0F;
-
-            return *this;
-        }
-
-        Color(const std::string& color)
-        {
-            uint32_t intValue = parseColorString(color);
-
-            r = static_cast<uint8_t>((intValue & 0xFF000000) >> 24) / 255.0F;
-            g = static_cast<uint8_t>((intValue & 0x00FF0000) >> 16) / 255.0F;
-            b = static_cast<uint8_t>((intValue & 0x0000FF00) >> 8) / 255.0F;
-            a = static_cast<uint8_t>(intValue & 0x000000FF) / 255.0F;
-        }
-
-        Color& operator=(const std::string& color)
-        {
-            uint32_t intValue = parseColorString(color);
-
-            r = static_cast<uint8_t>((intValue & 0xFF000000) >> 24) / 255.0F;
-            g = static_cast<uint8_t>((intValue & 0x00FF0000) >> 16) / 255.0F;
-            b = static_cast<uint8_t>((intValue & 0x0000FF00) >> 8) / 255.0F;
-            a = static_cast<uint8_t>(intValue & 0x000000FF) / 255.0F;
 
             return *this;
         }
