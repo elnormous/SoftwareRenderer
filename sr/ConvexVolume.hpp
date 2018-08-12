a36d509fe7023b313e2a31a4db1e5f124179c95e
Elviss Strazdins <elviss@elviss.lv>
2018-08-13 00:40:22 +0300

    Optimize the isBoxInside method Make Plane's methods const

--- sr/ConvexVolume.hpp
+++ sr/ConvexVolume.hpp
@@ -24,7 +24,7 @@ namespace sr
         {
         }
 
-        bool isPointInside(const Vector4F& position)
+        bool isPointInside(const Vector4F& position) const
         {
             for (const Plane& plane : planes)
             {
@@ -37,7 +37,7 @@ namespace sr
             return true;
         }
 
-        bool isSphereInside(const Vector4F& position, float radius)
+        bool isSphereInside(const Vector4F& position, float radius) const
         {
             for (const Plane& plane : planes)
             {
@@ -50,51 +50,29 @@ namespace sr
             return true;
         }
 
-        bool isBoxInside(const Box3& box)
+        bool isBoxInside(const Box3& box) const
         {
+            Vector4F leftBottomBack(box.min.v[0], box.min.v[1], box.min.v[2], 1.0F);
+            Vector4F leftBottomFront(box.min.v[0], box.min.v[1], box.max.v[2], 1.0F);
+            Vector4F leftTopBack(box.min.v[0], box.max.v[1], box.min.v[2], 1.0F);
+            Vector4F leftTopFront(box.min.v[0], box.max.v[1], box.max.v[2], 1.0F);
+            Vector4F rightBottomBack(box.max.v[0], box.min.v[1], box.min.v[2], 1.0F);
+            Vector4F rightBottomFront(box.max.v[0], box.min.v[1], box.max.v[2], 1.0F);
+            Vector4F rightTopBack(box.max.v[0], box.max.v[1], box.min.v[2], 1.0F);
+            Vector4F rightTopFront(box.max.v[0], box.max.v[1], box.max.v[2], 1.0F);
+
+
             for (const Plane& plane : planes)
             {
-                if (plane.dot(Vector4F(box.min.v[0], box.min.v[1], box.min.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.max.v[0], box.min.v[1], box.min.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.min.v[0], box.max.v[1], box.min.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.min.v[0], box.min.v[1], box.max.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.max.v[0], box.max.v[1], box.min.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.max.v[0], box.min.v[1], box.max.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.min.v[0], box.max.v[1], box.max.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                if (plane.dot(Vector4F(box.max.v[0], box.max.v[1], box.max.v[2], 1.0F)) >= 0.0F)
-                {
-                    continue;
-                }
-
-                return false;
+                if (plane.dot(leftBottomBack) < 0.0F &&
+                    plane.dot(leftBottomFront) < 0.0F &&
+                    plane.dot(leftTopBack) < 0.0F &&
+                    plane.dot(leftTopFront) < 0.0F &&
+                    plane.dot(rightBottomBack) < 0.0F &&
+                    plane.dot(rightBottomFront) < 0.0F &&
+                    plane.dot(rightTopBack) < 0.0F &&
+                    plane.dot(rightTopFront) < 0.0F)
+                    return false;
             }
 
             return true;
