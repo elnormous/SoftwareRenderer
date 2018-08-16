1d92160cf05388e608f2c5c6825aa36c5ed35834
Elviss Strazdins <elviss@elviss.lv>
2018-08-16 03:06:05 +0300

    Start implementing the Android build

---
+++ demo/app/src/main/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := demo
+LOCAL_CFLAGS := -Wall -Wextra
+LOCAL_CPPFLAGS += -std=c++11 -fexceptions
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../sr
+
+LOCAL_SRC_FILES := ../../../../Application.cpp \
+    ../../../../ApplicationAndroid.cpp
+
+LOCAL_LDLIBS := -llog -landroid -latomic
+
+include $(BUILD_SHARED_LIBRARY)
