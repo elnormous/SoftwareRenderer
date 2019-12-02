4bfa916d68c0b18d580b77c7467148a29d16278a
Elviss Strazdins <elviss@elviss.lv>
2019-12-03 01:12:34 +0200

    Allow Material copying
    Initialize textures to nullptr in class definition

--- sr/Material.hpp
+++ sr/Material.hpp
@@ -15,20 +15,11 @@ namespace sr
     class Material final
     {
     public:
-        Material()
-        {
-            std::fill(std::begin(textures), std::end(textures), nullptr);
-        }
-
-        Material(const Material&) = delete;
-        Material& operator=(const Material&) = delete;
-
-        Material(Material&&) = delete;
-        Material& operator=(Material&&) = delete;
+        Material() = default;
 
         //BlendState* blendState = nullptr;
         Shader* shader = nullptr;
-        Texture* textures[2];
+        Texture* textures[2]{nullptr, nullptr};
         //CullMode cullMode = CullMode::BACK;
         Color diffuseColor = Color::WHITE;
         float opacity = 1.0F;
