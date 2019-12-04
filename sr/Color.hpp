11b5d2c979489839207e2e40d7cba1e675062135
Elviss Strazdins <elviss@elviss.lv>
2019-12-04 03:41:28 +0200

    Makr functions of Color noexcept

--- sr/Color.hpp
+++ sr/Color.hpp
@@ -29,11 +29,11 @@ namespace sr
         float b = 0.0F;
         float a = 0.0F;
 
-        Color()
+        Color() noexcept
         {
         }
 
-        Color(uint32_t color):
+        Color(uint32_t color) noexcept:
             r(static_cast<uint8_t>((color & 0xFF000000) >> 24) / 255.0F),
             g(static_cast<uint8_t>((color & 0x00FF0000) >> 16) / 255.0F),
             b(static_cast<uint8_t>((color & 0x0000FF00) >> 8) / 255.0F),
@@ -41,7 +41,7 @@ namespace sr
         {
         }
 
-        Color& operator=(uint32_t color)
+        Color& operator=(uint32_t color) noexcept
         {
             r = static_cast<uint8_t>((color & 0xFF000000) >> 24) / 255.0F;
             g = static_cast<uint8_t>((color & 0x00FF0000) >> 16) / 255.0F;
@@ -51,12 +51,12 @@ namespace sr
             return *this;
         }
 
-        Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 0xFF):
+        Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 0xFF) noexcept:
             r(red / 255.0F), g(green / 255.0F), b(blue / 255.0F), a(alpha / 255.0F)
         {
         }
 
-        Color(float color[4]):
+        Color(float color[4]) noexcept:
             r(color[0]),
             g(color[1]),
             b(color[2]),
@@ -64,7 +64,7 @@ namespace sr
         {
         }
 
-        inline uint32_t getIntValue() const
+        inline uint32_t getIntValue() const noexcept
         {
             return (static_cast<uint32_t>(r * 255.0F) << 24) |
                    (static_cast<uint32_t>(g * 255.0F) << 16) |
@@ -72,7 +72,7 @@ namespace sr
                    static_cast<uint32_t>(a * 255.0F);
         }
 
-        inline uint32_t getIntValueRaw() const
+        inline uint32_t getIntValueRaw() const noexcept
         {
             uint32_t result;
             uint8_t* bytes = reinterpret_cast<uint8_t*>(&result);
