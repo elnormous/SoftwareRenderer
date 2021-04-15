ff5fa2f1aefdbf66c0dc7cc86c0e8d7790cf1033
Elviss Strazdins <elviss@elviss.lv>
2021-04-15 03:32:44 +0300

    Remake barycentric to a function template and take const references

--- sr/MathUtils.hpp
+++ sr/MathUtils.hpp
@@ -16,16 +16,17 @@ namespace sr
         return x < lo ? lo : x > hi ? hi : x;
     }
 
-    inline Vector<float, 3> barycentric(Vector<float, 2> a, Vector<float, 2> b, Vector<float, 2> c, Vector<float, 2> p) noexcept
+    template <class T>
+    inline Vector<T, 3> barycentric(const Vector<T, 2>& a, const Vector<T, 2>& b, const Vector<T, 2>& c, const Vector<T, 2>& p) noexcept
     {
-        const Vector<float, 3> side1{c.v[0] - a.v[0], b.v[0] - a.v[0], a.v[0] - p.v[0]};
-        const Vector<float, 3> side2{c.v[1] - a.v[1], b.v[1] - a.v[1], a.v[1] - p.v[1]};
+        const Vector<T, 3> side1{c.v[0] - a.v[0], b.v[0] - a.v[0], a.v[0] - p.v[0]};
+        const Vector<T, 3> side2{c.v[1] - a.v[1], b.v[1] - a.v[1], a.v[1] - p.v[1]};
         const auto u = side1.cross(side2);
 
-        if (std::abs(u.v[2]) < std::numeric_limits<float>::min()) // degenerate triangle (all three points in a line)
-            return Vector<float, 3>{-1.0F, 1.0F, 1.0F};
+        if (std::abs(u.v[2]) < std::numeric_limits<T>::min()) // degenerate triangle (all three points in a line)
+            return Vector<T, 3>{T(-1), T(1), T(1)};
 
-        return Vector<float, 3>{1.0F - (u.v[0] + u.v[1]) / u.v[2], u.v[1] / u.v[2], u.v[0] / u.v[2]};
+        return Vector<T, 3>{T(1) - (u.v[0] + u.v[1]) / u.v[2], u.v[1] / u.v[2], u.v[0] / u.v[2]};
     }
 }
 
